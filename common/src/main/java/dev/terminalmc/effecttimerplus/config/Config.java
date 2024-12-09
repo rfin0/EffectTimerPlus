e8388eae949d56d7c8c835ed253f0d93c4bd6b63
NotRyken <127091011+NotRyken@users.noreply.github.com>
2024-12-09 11:38:18 +0800

    Sync with Framework

--- common/src/main/java/dev/terminalmc/effecttimerplus/config/Config.java
+++ common/src/main/java/dev/terminalmc/effecttimerplus/config/Config.java
@@ -22,9 +22,8 @@ import dev.terminalmc.effecttimerplus.EffectTimerPlus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -33,6 +32,7 @@ public class Config {
     public final int version = 1;
     private static final Path DIR_PATH = Path.of("config");
     private static final String FILE_NAME = EffectTimerPlus.MOD_ID + ".json";
+    private static final String BACKUP_FILE_NAME = EffectTimerPlus.MOD_ID + ".unreadable.json";
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     // Options
@@ -130,21 +130,36 @@ public class Config {
         Config config = null;
         if (Files.exists(file)) {
             config = load(file, GSON);
+            if (config == null) {
+                backup();
+                EffectTimerPlus.LOG.warn("Resetting config");
+            }
         }
-        if (config == null) {
-            config = new Config();
-        }
-        return config;
+        return config != null ? config : new Config();
     }
 
     private static @Nullable Config load(Path file, Gson gson) {
-        try (FileReader reader = new FileReader(file.toFile())) {
+        try (InputStreamReader reader = new InputStreamReader(
+                new FileInputStream(file.toFile()), StandardCharsets.UTF_8)) {
             return gson.fromJson(reader, Config.class);
         } catch (Exception e) {
             // Catch Exception as errors in deserialization may not fall under
             // IOException or JsonParseException, but should not crash the game.
-            EffectTimerPlus.LOG.error("Unable to load config.", e);
+            EffectTimerPlus.LOG.error("Unable to load config", e);
             return null;
+        }
+    }
+
+    private static void backup() {
+        try {
+            EffectTimerPlus.LOG.warn("Copying {} to {}", FILE_NAME, BACKUP_FILE_NAME);
+            if (!Files.isDirectory(DIR_PATH)) Files.createDirectories(DIR_PATH);
+            Path file = DIR_PATH.resolve(FILE_NAME);
+            Path backupFile = file.resolveSibling(BACKUP_FILE_NAME);
+            Files.move(file, backupFile, StandardCopyOption.ATOMIC_MOVE,
+                    StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            EffectTimerPlus.LOG.error("Unable to copy config file", e);
         }
     }
 
@@ -154,8 +169,8 @@ public class Config {
             if (!Files.isDirectory(DIR_PATH)) Files.createDirectories(DIR_PATH);
             Path file = DIR_PATH.resolve(FILE_NAME);
             Path tempFile = file.resolveSibling(file.getFileName() + ".tmp");
-
-            try (FileWriter writer = new FileWriter(tempFile.toFile())) {
+            try (OutputStreamWriter writer = new OutputStreamWriter(
+                    new FileOutputStream(tempFile.toFile()), StandardCharsets.UTF_8)) {
                 writer.write(GSON.toJson(instance));
             } catch (IOException e) {
                 throw new IOException(e);
@@ -163,7 +178,7 @@ public class Config {
             Files.move(tempFile, file, StandardCopyOption.ATOMIC_MOVE,
                     StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
-            EffectTimerPlus.LOG.error("Unable to save config.", e);
+            EffectTimerPlus.LOG.error("Unable to save config", e);
         }
     }
 }
