0a249e5f15e18780c65a809b68687066fe23e0bd
NotRyken <127091011+NotRyken@users.noreply.github.com>
2025-01-29 08:40:36 +0800

    2025

--- common/src/main/java/dev/terminalmc/effecttimerplus/mixin/MixinGui.java
+++ common/src/main/java/dev/terminalmc/effecttimerplus/mixin/MixinGui.java
@@ -1,7 +1,7 @@
 /*
  * EffectTimerPlus
  * Copyright (C) 2024 magicus
- * Copyright (C) 2024 TerminalMC
+ * Copyright (C) 2025 TerminalMC
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
